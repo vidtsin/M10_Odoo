0b335a843ea7971f9567e3bdc660e44db042b96a
Yuhiro <powerofyuhir0@gmail.com>
2019-05-13 19:04:46 +0200

    tarea 2

--- addons/todo_task/models/todo_app.py
+++ addons/todo_task/models/todo_app.py
@@ -7,7 +7,7 @@ class TodoTask(models.Model):
     name = fields.Char('Description', required=True)
     is_done = fields.Boolean('Done?')
     active = fields.Boolean('Active?', default=True)
-    user_assigned = fields.Many2one('res.users', string="User Assigned", default=lambda s: s.env.user)
+    user_assigned = fields.Many2many('res.users', string="User Assigned", default=lambda s: s.env.user)
 
     @api.one
     def do_toggle_done(self):
